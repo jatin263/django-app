7f6c538a0a84eb2f50f3190cdc4496c9b4a997c9
Jatin Kumawat <jatinkumawat20@gmail.com>
2023-04-03 18:50:03 +0530

    5

---
+++ build_files.sh
@@ -0,0 +1,6 @@
+echo " Build Start"
+
+python3.9 -m pip install - requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+
+echo " Build Stop"
--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@ez-j7542%&pppwr-v(3p4@raw8_%_v#&8z0*=!k-sh#gu+1-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]','.vercel.app','.now.sh']
 
 
 # Application definition
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,"static"),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
